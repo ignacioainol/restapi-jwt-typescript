ce6f3b90810fdd1f7a7a2e8a28ceb19812451c52
Ignacio Ainol <iggnaxios@gmail.com>
2022-03-09 19:09:50 -0300

    created user model to queries

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,7 +1,16 @@
 import { Request, Response } from 'express';
+import User, { IUser } from '../models/User';
 
 export const signup = (req: Request, res: Response) => {
-    res.send('signup');
+
+    const user: IUser = new User({
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password,
+    })
+
+    console.log(user);
+    res.send(user);
 }
 
 export const signin = (req: Request, res: Response) => {
